28028e7387359dbdeef95b3731d75578b41bf6d2
Martin Beauchamp <martinb@shapeways.com>
2017-11-03 14:57:38 -0400

    add cidr_block outputs to public and private subnets

--- outputs.tf
+++ outputs.tf
@@ -25,9 +25,19 @@ output "private_subnets" {
   value       = ["${aws_subnet.private.*.id}"]
 }
 
+output "private_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of private subnets"
+  value       = ["${aws_subnet.private.*.cidr_block}"]
+}
+
 output "public_subnets" {
   description = "List of IDs of public subnets"
   value       = ["${aws_subnet.public.*.id}"]
+}
+
+output "public_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of public subnets"
+  value       = ["${aws_subnet.public.*.cidr_block}"]
 }
 
 output "database_subnets" {
