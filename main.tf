dd69a3b3c9af67798cb57ad2a65630f9b72d8f52
Martin Beauchamp <martinb@shapeways.com>
2017-11-28 11:00:47 -0500

    fix domain_name_servers - keep it a list

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_vpc_dhcp_options" "this" {
   count = "${var.enable_dhcp_options ? 1 : 0}"
 
   domain_name          = "${var.dhcp_options_domain_name}"
-  domain_name_servers  = "${var.dhcp_options_domain_name_servers}"
+  domain_name_servers  = [ "${var.dhcp_options_domain_name_servers}" ]
   ntp_servers          = "${var.dhcp_options_ntp_servers}"
   netbios_name_servers = "${var.dhcp_options_netbios_name_servers}"
   netbios_node_type    = "${var.dhcp_options_netbios_node_type}"
