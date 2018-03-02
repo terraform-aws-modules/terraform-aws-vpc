9b686be945afe50d65e439ee361526c39f1b48dc
Simon Clausen <github@simonclausen.dk>
2018-03-02 22:18:17 +0100

    Make terraform recognize lists when uring variables (#92)

--- main.tf
+++ main.tf
@@ -27,9 +27,9 @@ resource "aws_vpc_dhcp_options" "this" {
   count = "${var.create_vpc && var.enable_dhcp_options ? 1 : 0}"
 
   domain_name          = "${var.dhcp_options_domain_name}"
-  domain_name_servers  = "${var.dhcp_options_domain_name_servers}"
-  ntp_servers          = "${var.dhcp_options_ntp_servers}"
-  netbios_name_servers = "${var.dhcp_options_netbios_name_servers}"
+  domain_name_servers  = ["${var.dhcp_options_domain_name_servers}"]
+  ntp_servers          = ["${var.dhcp_options_ntp_servers}"]
+  netbios_name_servers = ["${var.dhcp_options_netbios_name_servers}"]
   netbios_node_type    = "${var.dhcp_options_netbios_node_type}"
 
   tags = "${merge(var.tags, var.dhcp_options_tags, map("Name", format("%s", var.name)))}"
