23b93bb0542391610ebd20ec822f7c97bb26e4a8
Anton Babenko <anton@antonbabenko.com>
2018-06-20 18:10:26 +0200

    Allow tags override for all resources (fix for #138)

--- examples/simple-vpc/main.tf
+++ examples/simple-vpc/main.tf
@@ -13,11 +13,19 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway = false
   single_nat_gateway = true
+
+  public_subnet_tags = {
+    Name = "overriden-name-public"
+  }
 
   tags = {
     Owner       = "user"
     Environment = "dev"
+  }
+
+  vpc_tags = {
+    Name = "vpc-name"
   }
 }
