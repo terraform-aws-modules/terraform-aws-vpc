d66fadda4cd4ff743ed43dd36d919f836600165c
jcastaldo08 <jcastaldo08@gmail.com>
2018-08-30 12:03:48 -0700

    From copy and paste accidentally used variable name that differed from these variables

--- examples/secondary-cidr-blocks/main.tf
+++ examples/secondary-cidr-blocks/main.tf
@@ -5,7 +5,7 @@ module "vpc" {
   source = "../../"
   name = "secondary-cidr-blocks-example"
   cidr = "10.0.0.0/16"
-  secondary_cidrs = ["10.1.0.0/16", "10.2.0.0/16"]
+  secondary_cidr_blocks = ["10.1.0.0/16", "10.2.0.0/16"]
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   private_subnets = ["10.0.1.0/24", "10.1.2.0/24", "10.2.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.1.102.0/24", "10.2.103.0/24"]
