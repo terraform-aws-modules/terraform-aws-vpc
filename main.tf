f3650e0df972c7b985426799190dfd3e7b431021
Mathias Bogaert <mathias.bogaert@gmail.com>
2018-12-05 21:50:55 +0000

    Select VPC when searching default security group.

--- main.tf
+++ main.tf
@@ -393,7 +393,7 @@ data "aws_vpc_endpoint_service" "kms" {
 data "aws_security_group" "default" {
   count = "${var.create_vpc && var.enable_kms_endpoint ? 1 : 0}"
 
-  vpc_id = "${aws_vpc.this.*.id}"
+  vpc_id = "${local.vpc_id}"
   name = "default"
 }
 
