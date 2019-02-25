6b833882a0c5417b5a1d66a415c361b999be727a
King Chung Huang <kinghuang@mac.com>
2019-02-25 11:50:15 -0700

    Restructure example config to use locals
    Restructure the network ACL rules in the network-acls example to use
    local variables to specify the rules, split between default and custom
    rules.

--- examples/network-acls/main.tf
+++ examples/network-acls/main.tf
@@ -13,101 +13,8 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  public_inbound_acl_rules = [
-    {
-      rule_number = 100
-      rule_action = "allow"
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_block  = "0.0.0.0/0"
-      description = "Allow inbound HTTP traffic from any IPv4 address"
-    },
-    {
-      rule_number = 110
-      rule_action = "allow"
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      cidr_block  = "0.0.0.0/0"
-      description = "Allow inbound HTTPS traffic from any IPv4 address"
-    },
-    {
-      rule_number = 120
-      rule_action = "allow"
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_block  = "0.0.0.0/0"
-      description = "Allow inbound SSH traffic from any IPv4 address"
-    },
-    {
-      rule_number = 130
-      rule_action = "allow"
-      from_port   = 3389
-      to_port     = 3389
-      protocol    = "tcp"
-      cidr_block  = "0.0.0.0/0"
-      description = "Allow inbound RDP traffic from any IPv4 address"
-    },
-    {
-      rule_number = 140
-      rule_action = "allow"
-      from_port   = 1024
-      to_port     = 65535
-      protocol    = "tcp"
-      cidr_block  = "0.0.0.0/0"
-      description = "Allow inbound return traffic from hosts"
-    },
-  ]
-
-  public_outbound_acl_rules = [
-    {
-      rule_number = 100
-      rule_action = "allow"
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_block  = "0.0.0.0/0"
-      description = "Allow outbound HTTP traffic from the subnet to the Internet"
-    },
-    {
-      rule_number = 110
-      rule_action = "allow"
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      cidr_block  = "0.0.0.0/0"
-      description = "Allow outbound HTTPS traffic from the subnet to the Internet"
-    },
-    {
-      rule_number = 120
-      rule_action = "allow"
-      from_port   = 1433
-      to_port     = 1433
-      protocol    = "tcp"
-      cidr_block  = "10.0.100.0/22"
-      description = "Allow outbound MS SQL access to database servers in the private subnet"
-    },
-    {
-      rule_number = 130
-      rule_action = "allow"
-      from_port   = 32768
-      to_port     = 65535
-      protocol    = "tcp"
-      cidr_block  = "0.0.0.0/0"
-      description = "Allows outbound responses to clients on the Internet"
-    },
-    {
-      rule_number = 140
-      rule_action = "allow"
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_block  = "10.0.100.0/22"
-      description = "Allows outbound SSH access to instances in your private subnet"
-    },
-  ]
+  public_inbound_acl_rules = "${concat(local.network_acls["default_inbound"], local.network_acls["public_inbound"])}"
+  public_outbound_acl_rules = "${concat(local.network_acls["default_outbound"], local.network_acls["public_outbound"])}"
 
   assign_generated_ipv6_cidr_block = true
 
@@ -125,5 +32,112 @@ module "vpc" {
 
   vpc_tags = {
     Name = "vpc-name"
+  }
+}
+
+locals {
+  network_acls = {
+    default_inbound = [
+      {
+        rule_number = 900
+        rule_action = "allow"
+        from_port   = 1024
+        to_port     = 65535
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+        description = "Allow inbound return traffic from hosts"
+      },
+    ]
+
+    default_outbound = [
+      {
+        rule_number = 900
+        rule_action = "allow"
+        from_port   = 32768
+        to_port     = 65535
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+        description = "Allows outbound responses to clients"
+      },
+    ]
+
+    public_inbound = [
+      {
+        rule_number = 100
+        rule_action = "allow"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+        description = "Allow inbound HTTP traffic from any IPv4 address"
+      },
+      {
+        rule_number = 110
+        rule_action = "allow"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+        description = "Allow inbound HTTPS traffic from any IPv4 address"
+      },
+      {
+        rule_number = 120
+        rule_action = "allow"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+        description = "Allow inbound SSH traffic from any IPv4 address"
+      },
+      {
+        rule_number = 130
+        rule_action = "allow"
+        from_port   = 3389
+        to_port     = 3389
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+        description = "Allow inbound RDP traffic from any IPv4 address"
+      },
+    ]
+
+    public_outbound = [
+      {
+        rule_number = 100
+        rule_action = "allow"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+        description = "Allow outbound HTTP traffic from the subnet to the Internet"
+      },
+      {
+        rule_number = 110
+        rule_action = "allow"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+        description = "Allow outbound HTTPS traffic from the subnet to the Internet"
+      },
+      {
+        rule_number = 120
+        rule_action = "allow"
+        from_port   = 1433
+        to_port     = 1433
+        protocol    = "tcp"
+        cidr_block  = "10.0.100.0/22"
+        description = "Allow outbound MS SQL access to database servers in the private subnet"
+      },
+
+      {
+        rule_number = 130
+        rule_action = "allow"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_block  = "10.0.100.0/22"
+        description = "Allows outbound SSH access to instances in your private subnet"
+      },
+    ]
   }
 }
