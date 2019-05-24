9127d84c1c002787e792d26b9ce85259ba16b0a7
Alex Goncharov <alex@goncharov.co>
2019-05-24 14:27:28 +0200

    fix outputs where coalescelist could be run with all parametes as empty lists

--- outputs.tf
+++ outputs.tf
@@ -196,20 +196,17 @@ output "private_route_table_ids" {
 
 output "database_route_table_ids" {
   description = "List of IDs of database route tables"
-  value       = coalescelist(aws_route_table.database.*.id, aws_route_table.private.*.id)
+  value       = length(aws_route_table.database.*.id) > 0 ? aws_route_table.database.*.id : aws_route_table.private.*.id
 }
 
 output "redshift_route_table_ids" {
   description = "List of IDs of redshift route tables"
-  value       = coalescelist(aws_route_table.redshift.*.id, aws_route_table.private.*.id)
+  value       = length(aws_route_table.redshift.*.id) > 0 ? aws_route_table.redshift.*.id : aws_route_table.private.*.id
 }
 
 output "elasticache_route_table_ids" {
   description = "List of IDs of elasticache route tables"
-  value = [coalescelist(
-    aws_route_table.elasticache.*.id,
-    aws_route_table.private.*.id,
-  )]
+  value       = length(aws_route_table.elasticache.*.id) > 0 ? aws_route_table.elasticache.*.id : aws_route_table.private.*.id
 }
 
 output "intra_route_table_ids" {
