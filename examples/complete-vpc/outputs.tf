fd2139b0b9bfb14388e8bd68168e7c2490ec3168
Alex Goncharov <alex@goncharov.co>
2019-05-24 10:12:32 +0200

    fix "list of lists" issued for examples outputs

--- examples/complete-vpc/outputs.tf
+++ examples/complete-vpc/outputs.tf
@@ -7,38 +7,38 @@ output "vpc_id" {
 # Subnets
 output "private_subnets" {
   description = "List of IDs of private subnets"
-  value       = [module.vpc.private_subnets]
+  value       = module.vpc.private_subnets
 }
 
 output "public_subnets" {
   description = "List of IDs of public subnets"
-  value       = [module.vpc.public_subnets]
+  value       = module.vpc.public_subnets
 }
 
 output "database_subnets" {
   description = "List of IDs of database subnets"
-  value       = [module.vpc.database_subnets]
+  value       = module.vpc.database_subnets
 }
 
 output "elasticache_subnets" {
   description = "List of IDs of elasticache subnets"
-  value       = [module.vpc.elasticache_subnets]
+  value       = module.vpc.elasticache_subnets
 }
 
 output "redshift_subnets" {
   description = "List of IDs of redshift subnets"
-  value       = [module.vpc.redshift_subnets]
+  value       = module.vpc.redshift_subnets
 }
 
 output "intra_subnets" {
   description = "List of IDs of intra subnets"
-  value       = [module.vpc.intra_subnets]
+  value       = module.vpc.intra_subnets
 }
 
 # NAT gateways
 output "nat_public_ips" {
   description = "List of public Elastic IPs created for AWS NAT Gateway"
-  value       = [module.vpc.nat_public_ips]
+  value       = module.vpc.nat_public_ips
 }
 
 # VPC endpoints
@@ -49,12 +49,12 @@ output "vpc_endpoint_ssm_id" {
 
 output "vpc_endpoint_ssm_network_interface_ids" {
   description = "One or more network interfaces for the VPC Endpoint for SSM."
-  value       = [module.vpc.vpc_endpoint_ssm_network_interface_ids]
+  value       = module.vpc.vpc_endpoint_ssm_network_interface_ids
 }
 
 output "vpc_endpoint_ssm_dns_entry" {
   description = "The DNS entries for the VPC Endpoint for SSM."
-  value       = [module.vpc.vpc_endpoint_ssm_dns_entry]
+  value       = module.vpc.vpc_endpoint_ssm_dns_entry
 }
 
 //
--- examples/simple-vpc/outputs.tf
+++ examples/simple-vpc/outputs.tf
@@ -7,7 +7,7 @@ output "vpc_id" {
 # CIDR blocks
 output "vpc_cidr_block" {
   description = "The CIDR block of the VPC"
-  value       = [module.vpc.vpc_cidr_block]
+  value       = module.vpc.vpc_cidr_block
 }
 
 //output "vpc_ipv6_cidr_block" {
@@ -18,23 +18,23 @@ output "vpc_cidr_block" {
 # Subnets
 output "private_subnets" {
   description = "List of IDs of private subnets"
-  value       = [module.vpc.private_subnets]
+  value       = module.vpc.private_subnets
 }
 
 output "public_subnets" {
   description = "List of IDs of public subnets"
-  value       = [module.vpc.public_subnets]
+  value       = module.vpc.public_subnets
 }
 
 # NAT gateways
 output "nat_public_ips" {
   description = "List of public Elastic IPs created for AWS NAT Gateway"
-  value       = [module.vpc.nat_public_ips]
+  value       = module.vpc.nat_public_ips
 }
 
 # AZs
 output "azs" {
   description = "A list of availability zones spefified as argument to this module"
-  value       = [module.vpc.azs]
+  value       = module.vpc.azs
 }
 
