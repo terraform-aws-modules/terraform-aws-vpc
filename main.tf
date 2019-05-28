b34a43516fe1dca714363494a9d6f9d433645181
Daniel Kimsey <dekimsey@gmail.com>
2019-05-27 19:46:54 -0500

    Update main.tf
    Co-Authored-By: Steffen Gebert <st+github@st-g.de>

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ resource "aws_internet_gateway" "this" {
 }
 
 resource "aws_egress_only_internet_gateway" "this" {
-  count = "${var.enable_ipv6 && length(var.database_subnets) > 0 && length(var.private_subnets) > 0 ? 1 : 0}"
+  count = "${var.enable_ipv6 && length(var.database_subnets) + length(var.private_subnets) > 0 ? 1 : 0}"
 
   vpc_id = "${aws_vpc.this.id}"
 }
