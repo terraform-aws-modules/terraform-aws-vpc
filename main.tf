5b20e5c523910183ed8c84409414232998aa1188
Steffen Gebert <steffen.gebert@emnify.com>
2019-07-08 23:36:14 +0200

    Revert unrelated change

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_vpc" "this" {
 resource "aws_vpc_ipv4_cidr_block_association" "this" {
   count = var.create_vpc && length(var.secondary_cidr_blocks) > 0 ? length(var.secondary_cidr_blocks) : 0
 
-  vpc_id = element(concat(aws_vpc.this.*.id, [""]), 0)
+  vpc_id = aws_vpc.this[0].id
 
   cidr_block = element(var.secondary_cidr_blocks, count.index)
 }
