83c98656ba91a47b1bb935a7c9b9564571a5c5c3
Anton Babenko <anton@antonbabenko.com>
2019-09-02 17:31:40 +0200

    Updated README

--- examples/complete-vpc/main.tf
+++ examples/complete-vpc/main.tf
@@ -27,7 +27,7 @@ module "vpc" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-  enable_nat_gateway = false
+  enable_nat_gateway = true
   single_nat_gateway = true
 
   enable_vpn_gateway = true
