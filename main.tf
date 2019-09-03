e452a170d9938c1fd43580a6494c7428f51c97f6
Anton Babenko <anton@antonbabenko.com>
2019-09-03 12:34:04 +0200

    Added ClassicLink

--- main.tf
+++ main.tf
@@ -33,6 +33,8 @@ resource "aws_vpc" "this" {
   instance_tenancy                 = var.instance_tenancy
   enable_dns_hostnames             = var.enable_dns_hostnames
   enable_dns_support               = var.enable_dns_support
+  enable_classiclink               = var.enable_classiclink
+  enable_classiclink_dns_support   = var.enable_classiclink_dns_support
   assign_generated_ipv6_cidr_block = var.enable_ipv6
 
   tags = merge(
