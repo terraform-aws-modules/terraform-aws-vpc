b521604030452ac3f0bef39503738c632e77f0ba
Erik Fredrickson <efredrickson@patientping.com>
2019-12-31 14:19:02 -0500

    added ignore tag changes directives

--- main.tf
+++ main.tf
@@ -37,6 +37,10 @@ resource "aws_vpc" "this" {
   enable_classiclink_dns_support   = var.enable_classiclink_dns_support
   assign_generated_ipv6_cidr_block = var.enable_ipv6
 
+  lifecycle {
+    ignore_changes = [tags]
+  }
+
   tags = merge(
     {
       "Name" = format("%s", var.name)
@@ -291,6 +295,10 @@ resource "aws_subnet" "public" {
 
   ipv6_cidr_block = var.enable_ipv6 && length(var.public_subnet_ipv6_prefixes) > 0 ? cidrsubnet(aws_vpc.this[0].ipv6_cidr_block, 8, var.public_subnet_ipv6_prefixes[count.index]) : null
 
+  lifecycle {
+    ignore_changes = [tags]
+  }
+
   tags = merge(
     {
       "Name" = format(
@@ -316,6 +324,10 @@ resource "aws_subnet" "private" {
   assign_ipv6_address_on_creation = var.private_subnet_assign_ipv6_address_on_creation == null ? var.assign_ipv6_address_on_creation : var.private_subnet_assign_ipv6_address_on_creation
 
   ipv6_cidr_block = var.enable_ipv6 && length(var.private_subnet_ipv6_prefixes) > 0 ? cidrsubnet(aws_vpc.this[0].ipv6_cidr_block, 8, var.private_subnet_ipv6_prefixes[count.index]) : null
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
 
   tags = merge(
     {
@@ -343,6 +355,10 @@ resource "aws_subnet" "database" {
 
   ipv6_cidr_block = var.enable_ipv6 && length(var.database_subnet_ipv6_prefixes) > 0 ? cidrsubnet(aws_vpc.this[0].ipv6_cidr_block, 8, var.database_subnet_ipv6_prefixes[count.index]) : null
 
+  lifecycle {
+    ignore_changes = [tags]
+  }
+
   tags = merge(
     {
       "Name" = format(
@@ -362,6 +378,10 @@ resource "aws_db_subnet_group" "database" {
   name        = lower(var.name)
   description = "Database subnet group for ${var.name}"
   subnet_ids  = aws_subnet.database.*.id
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
 
   tags = merge(
     {
@@ -385,6 +405,10 @@ resource "aws_subnet" "redshift" {
 
   ipv6_cidr_block = var.enable_ipv6 && length(var.redshift_subnet_ipv6_prefixes) > 0 ? cidrsubnet(aws_vpc.this[0].ipv6_cidr_block, 8, var.redshift_subnet_ipv6_prefixes[count.index]) : null
 
+  lifecycle {
+    ignore_changes = [tags]
+  }
+
   tags = merge(
     {
       "Name" = format(
@@ -404,6 +428,10 @@ resource "aws_redshift_subnet_group" "redshift" {
   name        = lower(var.name)
   description = "Redshift subnet group for ${var.name}"
   subnet_ids  = aws_subnet.redshift.*.id
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
 
   tags = merge(
     {
@@ -426,6 +454,10 @@ resource "aws_subnet" "elasticache" {
   assign_ipv6_address_on_creation = var.elasticache_subnet_assign_ipv6_address_on_creation == null ? var.assign_ipv6_address_on_creation : var.elasticache_subnet_assign_ipv6_address_on_creation
 
   ipv6_cidr_block = var.enable_ipv6 && length(var.elasticache_subnet_ipv6_prefixes) > 0 ? cidrsubnet(aws_vpc.this[0].ipv6_cidr_block, 8, var.elasticache_subnet_ipv6_prefixes[count.index]) : null
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
 
   tags = merge(
     {
@@ -460,6 +492,10 @@ resource "aws_subnet" "intra" {
   assign_ipv6_address_on_creation = var.intra_subnet_assign_ipv6_address_on_creation == null ? var.assign_ipv6_address_on_creation : var.intra_subnet_assign_ipv6_address_on_creation
 
   ipv6_cidr_block = var.enable_ipv6 && length(var.intra_subnet_ipv6_prefixes) > 0 ? cidrsubnet(aws_vpc.this[0].ipv6_cidr_block, 8, var.intra_subnet_ipv6_prefixes[count.index]) : null
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
 
   tags = merge(
     {
