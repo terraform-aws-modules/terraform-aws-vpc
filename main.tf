3326953195736e08a24791024dad8581c1bddf17
Guillaume GILL <guillaume.gill@smile.fr>
2020-04-19 14:39:49 +0200

    fix: #433 Add missing tags to EIGW

--- main.tf
+++ main.tf
@@ -106,6 +106,13 @@ resource "aws_egress_only_internet_gateway" "this" {
   count = var.create_vpc && var.enable_ipv6 && local.max_subnet_length > 0 ? 1 : 0
 
   vpc_id = local.vpc_id
+  tags   = merge(
+    {
+      "Name" = format("%s", var.name)
+    },
+    var.tags,
+    var.igw_tags,
+  )
 }
 
 ################
