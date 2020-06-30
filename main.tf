0e977df953c6a8822276923ebfc7497cdde5731e
Yujun Zhang <yujunz@nvidia.com>
2020-06-30 15:10:30 +0800

    Normalize aws_elasticache_subnet_group name

--- main.tf
+++ main.tf
@@ -267,7 +267,12 @@ resource "aws_subnet" "elasticache" {
 resource "aws_elasticache_subnet_group" "elasticache" {
   count = "${var.create_vpc && length(var.elasticache_subnets) > 0 && var.create_elasticache_subnet_group ? 1 : 0}"
 
-  name        = "${var.name}"
+  # Identifiers must begin with a letter; must contain only ASCII letters, digits, and hyphens; and must not end with a hyphen or contain two consecutive hyphens.
+  name = "${replace(replace(replace(var.name,
+     "/[[:^alnum:]]/", "-"),
+     "/-+/", "-"),
+     "/^[[:^alpha:]]*|-$/", "")}"
+
   description = "ElastiCache subnet group for ${var.name}"
   subnet_ids  = ["${aws_subnet.elasticache.*.id}"]
 }
