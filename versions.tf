5c4ca89aa003bbc024bb9d4ca811f35dccf05580
Thomas <thomas@sctr.net>
2020-08-17 16:55:19 -0700

    Terraform 0.13 upgrade

--- versions.tf
+++ versions.tf
@@ -2,6 +2,9 @@ terraform {
   required_version = ">= 0.12.7, < 0.14"
 
   required_providers {
-    aws = ">= 2.68, < 4.0"
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 2.68, < 4.0"
+    }
   }
 }
