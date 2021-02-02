a7cef61c2f43835079280127ff7a6fe4cd75323d
andormarkus <andormarkus@marc-o-polo.com>
2021-02-02 19:53:01 +0100

    fix: vpc endpoint service s3

--- vpc-endpoints.tf
+++ vpc-endpoints.tf
@@ -4,7 +4,9 @@
 data "aws_vpc_endpoint_service" "s3" {
   count = var.create_vpc && var.enable_s3_endpoint ? 1 : 0
 
-  service = "s3"
+  service      = "s3"
+  service_type = "Gateway"
+
 }
 
 resource "aws_vpc_endpoint" "s3" {
