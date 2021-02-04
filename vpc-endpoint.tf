691737859e2aaf45e0a84760a87ee9829e0229e3
Anton Babenko <anton@antonbabenko.com>
2021-02-04 10:30:16 +0100

    fix: Fixed multiple VPC endpoint error for S3

--- vpc-endpoint.tf
+++ vpc-endpoint.tf
@@ -1,17 +1,22 @@
 ######################
 # VPC Endpoint for S3
 ######################
-data "aws_vpc_endpoint_service" "s3" {
-  count = "${var.create_vpc && var.enable_s3_endpoint ? 1 : 0}"
+# Since 2.2.2021 there is an error:
+# Error: multiple VPC Endpoint Services matched; use additional constraints to reduce matches to a single VPC Endpoint Service
+# The solution is to not use aws_vpc_endpoint_service datasource, but use service_name explicitly in aws_vpc_endpoint.
+#data "aws_vpc_endpoint_service" "s3" {
+#  count = "${var.create_vpc && var.enable_s3_endpoint ? 1 : 0}"
+#
+#  service = "s3"
+#}
 
-  service = "s3"
-}
+data "aws_region" "current" {}
 
 resource "aws_vpc_endpoint" "s3" {
   count = "${var.create_vpc && var.enable_s3_endpoint ? 1 : 0}"
 
   vpc_id       = "${local.vpc_id}"
-  service_name = "${data.aws_vpc_endpoint_service.s3.service_name}"
+  service_name = "com.amazonaws.${data.aws_region.current.name}.s3"
 
   tags = "${local.vpce_tags}"
 }
