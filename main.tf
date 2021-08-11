e816075fb5cfc821534bed7d10071e798834c912
Vivekanandan Rajendran <vivekanandan.rajendran@youlend.com>
2021-08-11 17:02:32 +0100

    removed additonal outpost subnet entry

--- main.tf
+++ main.tf
@@ -450,35 +450,6 @@ resource "aws_subnet" "private_eks" {
 }
 
 ################################################################################
-# Outpost subnet
-################################################################################
-
-resource "aws_subnet" "outpost" {
-  count = var.create_vpc && length(var.outpost_subnets) > 0 ? length(var.outpost_subnets) : 0
-
-  vpc_id                          = local.vpc_id
-  cidr_block                      = var.outpost_subnets[count.index]
-  availability_zone               = var.outpost_az
-  assign_ipv6_address_on_creation = var.outpost_subnet_assign_ipv6_address_on_creation == null ? var.assign_ipv6_address_on_creation : var.outpost_subnet_assign_ipv6_address_on_creation
-
-  ipv6_cidr_block = var.enable_ipv6 && length(var.outpost_subnet_ipv6_prefixes) > 0 ? cidrsubnet(aws_vpc.this[0].ipv6_cidr_block, 8, var.outpost_subnet_ipv6_prefixes[count.index]) : null
-
-  outpost_arn = var.outpost_arn
-
-  tags = merge(
-    {
-      "Name" = format(
-        "%s-${var.outpost_subnet_suffix}-%s",
-        var.name,
-        var.outpost_az,
-      )
-    },
-    var.tags,
-    var.outpost_subnet_tags,
-  )
-}
-
-################################################################################
 # Database subnet
 ##################
 resource "aws_subnet" "database" {
