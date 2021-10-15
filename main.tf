ad1b7cfb9cf9d0f1a123fc87edb51f350a60ab1a
Mukesh Sharma <mukeshsharma24@gmail.com>
2021-10-15 20:47:27 +1300

    feat: Added timeout block to aws_default_route_table resource (#701)

--- main.tf
+++ main.tf
@@ -205,6 +205,11 @@ resource "aws_default_route_table" "default" {
     }
   }
 
+  timeouts {
+    create = "5m"
+    update = "5m"
+  }
+
   tags = merge(
     { "Name" = var.name },
     var.tags,
