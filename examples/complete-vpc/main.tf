668d457a48789f461da79a3912ecc5f4ed8c6fd4
syamkumar <hey@syam.dev>
2022-01-27 22:56:23 +0530

    chore: Fixed code in examples (dynamodb resource not found error) (#742)

--- examples/complete-vpc/main.tf
+++ examples/complete-vpc/main.tf
@@ -208,7 +208,7 @@ data "aws_iam_policy_document" "dynamodb_endpoint_policy" {
       test     = "StringNotEquals"
       variable = "aws:sourceVpce"
 
-      values = [data.aws_vpc_endpoint.dynamodb.id]
+      values = [data.vpc.vpc_id]
     }
   }
 }
