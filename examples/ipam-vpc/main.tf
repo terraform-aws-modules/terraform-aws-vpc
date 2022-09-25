f043552e7d73ad5a50e4834e66ff57b8e4a99edd
Bryant Biggs <bryantbiggs@gmail.com>
2022-09-25 08:56:29 -0400

    chore: Updates from testing and validating IPAM example

--- examples/ipam-vpc/main.tf
+++ examples/ipam-vpc/main.tf
@@ -6,7 +6,7 @@ locals {
   name   = "ex-${replace(basename(path.cwd), "_", "-")}"
   region = "eu-west-1"
 
-  partition = cidrsubnets(data.aws_vpc_ipam_preview_next_cidr.this.cidr, 2, 2)
+  partition = cidrsubnets(aws_vpc_ipam_preview_next_cidr.this.cidr, 2, 2)
 
   tags = {
     Example    = local.name
@@ -19,25 +19,6 @@ locals {
 # VPC Module
 ################################################################################
 
-/*
-NOTES ON IPAM USAGE:
-
-In order to build subnets with your VPC Terraform must
-know subnet CIDRs to properly plan # of resources to build.
-Since CIDR is derived by IPAM by calling CreateVpc this is not
-possible within a module unless cidr is known ahead of time.
-We can get around this by "previewing" the CIDR and then using that
-as the subnet values.
-
-In the example below we use `cidrsubnets()` to calculate a public
-and private "partitions" (group of cidrs) then calculate the specific
-CIDRs for each subnet type.
-
-For an explanation on prolonged delete times on IPAM pools see 2nd
-*note* in terraform docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_ipam_pool_cidr
-*/
-
-
 module "vpc" {
   source = "../.."
 
@@ -48,7 +29,7 @@ module "vpc" {
 
   ipv4_ipam_pool_id = aws_vpc_ipam_pool.this.id
   azs               = ["${local.region}a", "${local.region}b"]
-  cidr              = data.aws_vpc_ipam_preview_next_cidr.this.cidr
+  cidr              = aws_vpc_ipam_preview_next_cidr.this.cidr
 
   tags = local.tags
 }
@@ -56,6 +37,20 @@ module "vpc" {
 ################################################################################
 # Supporting Resources
 ################################################################################
+
+/*
+NOTES ON IPAM USAGE:
+
+In order to build subnets with your VPC Terraform must know subnet CIDRs to properly plan # of resources to build.
+Since CIDR is derived by IPAM by calling CreateVpc this is not possible within a module unless cidr is known ahead of time.
+We can get around this by "previewing" the CIDR and then using that as the subnet values.
+
+In the example below we use `cidrsubnets()` to calculate a public and private "partitions" (group of cidrs) then calculate the specific
+CIDRs for each subnet type.
+
+For an explanation on prolonged delete times on IPAM pools see 2nd
+*note* in terraform docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_ipam_pool_cidr
+*/
 
 resource "aws_vpc_ipam" "this" {
   operating_regions {
@@ -67,15 +62,19 @@ resource "aws_vpc_ipam_pool" "this" {
   address_family                    = "ipv4"
   ipam_scope_id                     = aws_vpc_ipam.this.private_default_scope_id
   locale                            = local.region
-  allocation_default_netmask_length = 28
+  allocation_default_netmask_length = 24
 }
 
 resource "aws_vpc_ipam_pool_cidr" "this" {
   ipam_pool_id = aws_vpc_ipam_pool.this.id
-  cidr         = "172.2.0.0/16"
+  cidr         = "10.0.0.0/16"
 }
 
-data "aws_vpc_ipam_preview_next_cidr" "this" {
+resource "aws_vpc_ipam_preview_next_cidr" "this" {
   ipam_pool_id   = aws_vpc_ipam_pool.this.id
-  netmask_length = 24
+  netmask_length = 20
+
+  depends_on = [
+    aws_vpc_ipam_pool_cidr.this
+  ]
 }
