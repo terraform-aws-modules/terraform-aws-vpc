4fe7745ddb675af3bd50daf335ad3ffa16d08a98
drewmullen <drmullen@amazon.com>
2022-09-26 10:48:40 -0400

    feat: Add IPAM IPv6 support (#718)
    Co-authored-by: Bryant Biggs <bryantbiggs@gmail.com>

--- main.tf
+++ main.tf
@@ -20,15 +20,20 @@ locals {
 resource "aws_vpc" "this" {
   count = local.create_vpc ? 1 : 0
 
-  cidr_block        = var.cidr
-  ipv4_ipam_pool_id = var.ipv4_ipam_pool_id
+  cidr_block          = var.use_ipam_pool ? null : var.cidr
+  ipv4_ipam_pool_id   = var.ipv4_ipam_pool_id
+  ipv4_netmask_length = var.ipv4_netmask_length
 
-  instance_tenancy                 = var.instance_tenancy
-  enable_dns_hostnames             = var.enable_dns_hostnames
-  enable_dns_support               = var.enable_dns_support
-  enable_classiclink               = null # https://github.com/hashicorp/terraform/issues/31730
-  enable_classiclink_dns_support   = null # https://github.com/hashicorp/terraform/issues/31730
-  assign_generated_ipv6_cidr_block = var.enable_ipv6
+  assign_generated_ipv6_cidr_block = var.enable_ipv6 && !var.use_ipam_pool ? true : null
+  ipv6_cidr_block                  = var.ipv6_cidr
+  ipv6_ipam_pool_id                = var.ipv6_ipam_pool_id
+  ipv6_netmask_length              = var.ipv6_netmask_length
+
+  instance_tenancy               = var.instance_tenancy
+  enable_dns_hostnames           = var.enable_dns_hostnames
+  enable_dns_support             = var.enable_dns_support
+  enable_classiclink             = null # https://github.com/hashicorp/terraform/issues/31730
+  enable_classiclink_dns_support = null # https://github.com/hashicorp/terraform/issues/31730
 
   tags = merge(
     { "Name" = var.name },
