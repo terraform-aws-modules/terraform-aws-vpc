48447d39129da9e8eaacc621167c95555e9e06a3
air3ijai <88528265+air3ijai@users.noreply.github.com>
2022-10-13 11:55:54 +0300

    Use default `cidr` value from RFC 1918 (#839)

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "name" {
 variable "cidr" {
   description = "(Optional) The IPv4 CIDR block for the VPC. CIDR can be explicitly set or it can be derived from IPAM using `ipv4_netmask_length` & `ipv4_ipam_pool_id`"
   type        = string
-  default     = "0.0.0.0/0"
+  default     = "10.0.0.0/16"
 }
 
 variable "enable_ipv6" {
