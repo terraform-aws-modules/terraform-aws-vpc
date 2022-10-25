a7bee3586dfb7bb6796a3fdf431d1170894ac571
SlavaSubotskiy <anandasattva@gmail.com>
2022-10-25 17:03:01 +0300

    network-acl: fix deps usage

--- modules/network-acl/outputs.tf
+++ modules/network-acl/outputs.tf
@@ -1,9 +1,9 @@
 output "network_acl_id" {
-  value       = aws_network_acl.this.id
+  value       = aws_network_acl.this[0].id
   description = "The ID of the network ACL"
 }
 
 output "network_acl_arn" {
-  value       = aws_network_acl.this.arn
+  value       = aws_network_acl.this[0].arn
   description = "The ARN of the network ACL"
 }
