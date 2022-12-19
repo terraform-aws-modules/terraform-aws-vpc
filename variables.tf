c47c04b55c708d4b3d84057de04dfc6a4f350523
Anuj Nautiyal <nautiyalanuj6@gmail.com>
2022-12-19 15:44:55 +0530

    Update CIRC variable data type to list(string)
    Update the CIDR variable data type to list(string) to support attachment of additional CIDR blocks to current VPC

--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "name" {
 
 variable "cidr" {
   description = "(Optional) The IPv4 CIDR block for the VPC. CIDR can be explicitly set or it can be derived from IPAM using `ipv4_netmask_length` & `ipv4_ipam_pool_id`"
-  type        = string
+  type        = list(string)
   default     = "0.0.0.0/0"
 }
 
