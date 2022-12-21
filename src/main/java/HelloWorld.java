78e1aea9d179fbe5c69e875658f8e688d937bf0a
Ubuntu <ubuntu@ip-172-31-25-52.ec2.internal>
2022-12-21 13:53:56 +0000

    sparkjawa

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+import spark.servlet.SparkApplication;
+
+import static spark.Spark.get;
+
+public class HelloWorld implements SparkApplication {
+	public static void main(String[] args) {
+		new HelloWorld().init();
+	}
+
+	@Override
+	public void init() {
+		get("/hello", (req, res) -> "Hello World");
+	}
+}
