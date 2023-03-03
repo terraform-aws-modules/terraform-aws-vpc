3b3b180ce0264a14494ba371021f7c89358a5e10
Brian Carpio <briataws@amazon.com>
2023-03-03 14:17:22 -0800

    revering example to origional

--- examples/secondary-cidr-blocks/main.tf
+++ examples/secondary-cidr-blocks/main.tf
@@ -25,9 +25,9 @@ module "vpc" {
   cidr                  = "10.0.0.0/16"
   secondary_cidr_blocks = ["10.1.0.0/16", "10.2.0.0/16"]
 
-  azs                 = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  private_subnets     = ["10.0.1.0/24", "10.1.2.0/24", "10.2.3.0/24"]
-  public_subnets      = ["10.0.101.0/24", "10.1.102.0/24", "10.2.103.0/24"]
+  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  private_subnets = ["10.0.1.0/24", "10.1.2.0/24", "10.2.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.1.102.0/24", "10.2.103.0/24"]
 
   enable_ipv6 = true
 
