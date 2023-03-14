6b2af26187770582974f9e6a5d8a190114d5ed93
Saed SayedAhmed <34514167+saedx1@users.noreply.github.com>
2023-03-14 14:48:46 -0400

    Fix typo in complete-vpc

--- examples/complete-vpc/main.tf
+++ examples/complete-vpc/main.tf
@@ -208,7 +208,7 @@ data "aws_iam_policy_document" "dynamodb_endpoint_policy" {
 
     condition {
       test     = "StringNotEquals"
-      variable = "aws:sourceVpce"
+      variable = "aws:sourceVpc"
 
       values = [module.vpc.vpc_id]
     }
