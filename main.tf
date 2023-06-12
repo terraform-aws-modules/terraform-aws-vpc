06f8f113cebc6e497907da2a799c5056687c3a69
Arathy sadasivan Nair <99047478+arathysn@users.noreply.github.com>
2023-06-12 12:45:38 +0100

    Update main.tf
    Modifying the "aws_eip" resource

--- main.tf
+++ main.tf
@@ -1297,7 +1297,7 @@ locals {
 resource "aws_eip" "nat" {
   count = var.create_vpc && var.enable_nat_gateway && false == var.reuse_nat_ips ? local.nat_gateway_count : 0
 
-  vpc = true
+  domain = "vpc"
 
   tags = merge(
     {
