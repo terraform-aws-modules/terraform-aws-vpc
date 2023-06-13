d484fe6617b92abb2cf4f7f7b084d144eed1cb5f
Arathy sadasivan Nair <99047478+arathysn@users.noreply.github.com>
2023-06-13 08:57:12 +0100

    Update main.tf
    Correcting the code

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ locals {
   # Use `local.vpc_id` to give a hint to Terraform that subnets should be deleted before secondary CIDR blocks can be free!
   vpc_id = try(aws_vpc_ipv4_cidr_block_association.this[0].vpc_id, aws_vpc.this[0].id, "")
 
-  create_vpc = var.create_vpc && var.putin_khuylo
+  create_vpc = var.create_vpc
 }
 
 ################################################################################
