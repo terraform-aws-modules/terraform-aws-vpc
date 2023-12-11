6e25437b16955b9393348d91965ead2f755fb2e0
Kasey Linden <kaseylinden@gmail.com>
2023-12-11 09:34:22 -0600

    feat: Add Cross Account Flow Support (#1014)
    * feat: Add Cross Account Flow Support
    
    * fix: update flow log naming convention
    
    ---------
    
    Co-authored-by: Kasey Linden <klinden@vmware.com>
    Co-authored-by: Bryant Biggs <bryantbiggs@gmail.com>

--- vpc-flow-logs.tf
+++ vpc-flow-logs.tf
@@ -17,13 +17,14 @@ locals {
 resource "aws_flow_log" "this" {
   count = local.enable_flow_log ? 1 : 0
 
-  log_destination_type     = var.flow_log_destination_type
-  log_destination          = local.flow_log_destination_arn
-  log_format               = var.flow_log_log_format
-  iam_role_arn             = local.flow_log_iam_role_arn
-  traffic_type             = var.flow_log_traffic_type
-  vpc_id                   = local.vpc_id
-  max_aggregation_interval = var.flow_log_max_aggregation_interval
+  log_destination_type       = var.flow_log_destination_type
+  log_destination            = local.flow_log_destination_arn
+  log_format                 = var.flow_log_log_format
+  iam_role_arn               = local.flow_log_iam_role_arn
+  deliver_cross_account_role = var.flow_log_deliver_cross_account_role
+  traffic_type               = var.flow_log_traffic_type
+  vpc_id                     = local.vpc_id
+  max_aggregation_interval   = var.flow_log_max_aggregation_interval
 
   dynamic "destination_options" {
     for_each = var.flow_log_destination_type == "s3" ? [true] : []
