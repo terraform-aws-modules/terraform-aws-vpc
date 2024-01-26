d7a3d2049b6e15b7a1960dcbe055c1f078c66ef9
emrht <emrhtfn@gmail.com>
2024-01-26 16:26:38 +0300

    initial commit

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.0"
 
+  backend "s3" {
+    bucket         = "terraform-aws-vpc-terraform-state-prod-597947213367-eu-west-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+    key            = "prod/eu-west-1/prod/terraform-aws-vpc/terraform.tfstate"
+    region         = "eu-west-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
