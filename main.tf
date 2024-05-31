e571206b5e7a2907cb3eca8daffeb899f138bbdf
coley-angel <106187439+coley-angel@users.noreply.github.com>
2024-05-31 16:43:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -155,7 +155,7 @@ resource "aws_route" "public_internet_gateway" {
   count = local.create_public_subnets && var.create_igw ? 1 : 0
 
   route_table_id         = aws_route_table.public[0].id
-  destination_cidr_block = "0.0.0.0/0"
+  destination_cidr_block = "10.0.0.0/8"
   gateway_id             = aws_internet_gateway.this[0].id
 
   timeouts {
