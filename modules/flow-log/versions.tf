1c561a729afcfaab05fb2c50091a6f492ceac5b6
Bryant Biggs <bryantbiggs@gmail.com>
2026-01-08 13:15:54 -0600

    feat: Add provider meta user-agent (#1272)
    * feat: Add provider meta user-agent
    
    * fix: Update provider to support longer user-agent tag

--- modules/flow-log/versions.tf
+++ modules/flow-log/versions.tf
@@ -4,7 +4,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.5"
+      version = ">= 6.28"
     }
+  }
+
+  provider_meta "aws" {
+    user_agent = [
+      "github.com/terraform-aws-modules/terraform-aws-vpc"
+    ]
   }
 }
